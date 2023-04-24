45cfffbc8c03805315a0ccba5a841e7af0f0b3ae
mmesander <mark_mesander@hotmail.com>
2023-04-24 22:18:32 +0200

    De functie van de password-error verbeterd

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const usernameField = document.getElementById('username-field');
-
 const usernameError = document.getElementById('username-error-message');
 
 const passwordField = document.getElementById('password-field');
-
-const passwordError = document.getElementById('password-error-message')
+const passwordError = document.getElementById('password-error-message');
 
 function handleInputUsername(e) {
     if (e.target.value.includes('@')){
-        usernameError.textContent = "Gebruikersnaam mag geen @ bevatten"
-    } else usernameError.textContent = ""
+        usernameError.textContent = "Gebruikersnaam mag geen @ bevatten";
+    } else usernameError.textContent = "";
 }
 
 function handleInputPassword(e) {
-    if (e.target.value.length < 7) {
-        passwordError.textContent = "Wachtwoord is te kort, gebruik minimaal 6 tekens"
-    } else passwordError.textContent = ""
+    if (e.target.value === "" || e.target.value.length > 6) {
+        passwordError.textContent = "";
+    } else { passwordError.textContent = "Wachtwoord is te kort, gebruik minimaal 6 tekens";
+    }
+
+
 }
 
 usernameField.addEventListener('keyup', handleInputUsername)
