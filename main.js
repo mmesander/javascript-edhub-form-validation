be71dae22fd528baa4c44dee650e55ee22dae322
mmesander <mark_mesander@hotmail.com>
2023-04-24 22:59:51 +0200

    Javascript code iets beter geordend

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ const usernameError = document.getElementById('username-error-message');
 const passwordField = document.getElementById('password-field');
 const passwordError = document.getElementById('password-error-message');
 
+usernameField.addEventListener('keyup', handleInputUsername)
+passwordField.addEventListener('keyup', handleInputPassword)
+
 function handleInputUsername(e) {
-    if (e.target.value.includes('@')){
+    if (e.target.value.includes('@')) {
         usernameError.textContent = "Gebruikersnaam mag geen @ bevatten";
     } else usernameError.textContent = "";
 }
@@ -13,12 +16,10 @@ function handleInputUsername(e) {
 function handleInputPassword(e) {
     if (e.target.value === "" || e.target.value.length > 6) {
         passwordError.textContent = "";
-    } else { passwordError.textContent = "Wachtwoord is te kort, gebruik minimaal 6 tekens";
+    } else {
+        passwordError.textContent = "Wachtwoord is te kort, gebruik minimaal 6 tekens";
     }
-
-
 }
 
-usernameField.addEventListener('keyup', handleInputUsername)
 
-passwordField.addEventListener('keyup', handleInputPassword)
+
